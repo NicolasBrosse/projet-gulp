b3e0e0f427821b6653c33beffdc9596d7d6520a3
Nicolas Brosse <nicolas@nicolasbrosse.fr>
2016-04-18 17:25:48 +0200

    ajout de dépendances dans le package

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),
+    rename = require("gulp-rename"),
+    livereload = require('gulp-livereload'),
+    watch = require('gulp-watch');
 
+gulp.task('watch', function () {
+   gulp.watch('app/*', ['minify']);
+});
+
+// Minifie les fichiers HTML
+gulp.task('minify', function() {
+  return gulp.src('app/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(rename(function (path) {
+        path.basename += "-mini";
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+
+// Tache par défaut de Gulp
 gulp.task('default', function () {
 
 });
